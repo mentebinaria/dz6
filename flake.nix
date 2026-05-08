6a57a12bc1e3a99d98573967c92cfbbb28c11182
Gutem <gutem@thehou.se>
2026-05-08 02:22:10 -0300

    feat: add Homebrew, Nix, and CI workflows for release automation

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Fast Vim-inspired TUI hex editor";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "dz6";
+          version = "0.7.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            rust-analyzer
+            cargo-watch
+            cargo-edit
+          ];
+        };
+      }
+    );
+}
