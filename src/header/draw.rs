3c6b66c3b7750a036a741f465de04bd0452a7e5e
merces <nandu88@gmail.com>
2026-07-31 19:48:35 -0300

    Add a message for unsupported file formats in header view

--- src/header/draw.rs
+++ src/header/draw.rs
@@ -1,4 +1,8 @@
-use ratatui::{Frame, layout::Rect};
+use ratatui::{
+    Frame,
+    layout::{Constraint, Rect},
+    text::Text,
+};
 
 use crate::{app::App, header::formats};
 
@@ -7,5 +11,9 @@ pub fn header_contents_draw(app: &mut App, frame: &mut Frame, area: Rect) {
         formats::pe::draw::pe_draw(app, frame, area);
     } else if app.file_info.r#type == "ELF" {
         formats::elf::draw::elf_draw(app, frame, area);
+    } else {
+        let message = Text::from("No header view available for this file format").centered();
+
+        frame.render_widget(message, area.centered_vertically(Constraint::Ratio(1, 4)));
     }
 }
